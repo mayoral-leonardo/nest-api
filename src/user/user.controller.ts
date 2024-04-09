f78367289003e2ef8c88d4c55ab74c2b824c8dda
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2024-04-09 20:36:10 -0300

    feat: added prisma config and create user route now works

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,12 +12,15 @@ import {
 import { CreateUserDTO } from './dto/create-user-dto';
 import { UpdatePutUserDTO } from './dto/update-put-user-dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user-dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   async create(@Body() body: CreateUserDTO) {
-    return { body };
+    return this.userService.create(body);
   }
 
   @Get()
