6d63137993752c512acf82cc92d5e5c6d8aa4d4b
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2024-04-08 21:03:11 -0300

    Initialized project

---
+++ src/user/user.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Patch,
+  Body,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user-dto';
+import { UpdatePutUserDTO } from './dto/update-put-user-dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user-dto';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body: CreateUserDTO) {
+    return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param('id', ParseIntPipe) id: number) {
+    return { user: {}, id };
+  }
+
+  @Put(':id')
+  async update(
+    @Body() body: UpdatePutUserDTO,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return {
+      method: 'put',
+      body,
+      id,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(
+    @Body() body: UpdatePatchUserDTO,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return {
+      method: 'patch',
+      body,
+      id,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return {
+      id,
+    };
+  }
+}
