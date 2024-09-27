c3aab01090c5c5d7a3e5ea4ebf291f4f89a4dabd
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2024-09-26 21:14:31 -0300

    test: add auth controller test

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthGuard } from '../guards/auth.guard';
+import { guardMock } from '../testing/guard.mock';
+import { AuthController } from './auth.controller';
+import { authServiceMock } from '../testing/auth-service.mock';
+
+describe('AuthController', () => {
+  let authController: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [authServiceMock],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(guardMock)
+      .compile();
+
+    authController = module.get<AuthController>(AuthController);
+  });
+
+  test('Validate definitions', () => {
+    expect(authController).toBeDefined();
+  });
+});
