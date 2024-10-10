132c2b95059fe3e5302958bfdaaffa6af2f1fd87
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2024-10-10 17:04:45 -0300

    feat: add new measuring data methods

--- src/measuringData/measuringData.controller.ts
+++ src/measuringData/measuringData.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Put,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ParamId } from '../decorators/param-id.decorator';
 import { MeasuringDataRegisterDTO } from './dto/measuring-data-register';
+import { MeasuringDataUpdatePatchDTO } from './dto/measuring-data-update-patch';
+import { MeasuringDataUpdatePutDTO } from './dto/measuring-data-update-put';
 import { MeasuringDataService } from './measuringData.service';
 
 @Controller('measuringData')
@@ -20,5 +31,42 @@ export class MeasuringDataController {
   @Get()
   async list() {
     return this.measuringDataService.list();
+  }
+
+  @Get(':id')
+  async show(@ParamId() id: number) {
+    return this.measuringDataService.show(id);
+  }
+
+  @Put(':id')
+  async update(@Body() body: MeasuringDataUpdatePutDTO, @ParamId() id: number) {
+    const data = {
+      ...body,
+      equipment_ID: Number(body.equipment_ID),
+      user_ID: Number(body.user_ID),
+      value: Number(body.value),
+    };
+    return this.measuringDataService.update(id, data);
+  }
+
+  @Patch(':id')
+  async updatePartial(
+    @Body() body: MeasuringDataUpdatePatchDTO,
+    @ParamId() id: number,
+  ) {
+    const data = {
+      ...body,
+      equipment_ID: Number(body.equipment_ID),
+      user_ID: Number(body.user_ID),
+      value: Number(body.value),
+    };
+    return this.measuringDataService.updatePartial(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@ParamId() id: number) {
+    return {
+      success: await this.measuringDataService.delete(id),
+    };
   }
 }
--- src/measuringData/measuringData.service.ts
+++ src/measuringData/measuringData.service.ts
@@ -1,6 +1,8 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { MeasuringDataRegisterDTO } from './dto/measuring-data-register';
 import { PrismaService } from '../prisma/prisma.service';
+import { MeasuringDataUpdatePutDTO } from './dto/measuring-data-update-put';
+import { MeasuringDataUpdatePatchDTO } from './dto/measuring-data-update-patch';
 
 @Injectable()
 export class MeasuringDataService {
@@ -20,5 +22,74 @@ export class MeasuringDataService {
 
   async list() {
     return await this.prisma.measurement.findMany();
+  }
+
+  async show(id: number) {
+    await this.exists(id);
+
+    return await this.prisma.measurement.findUnique({
+      where: {
+        measurement_ID: id,
+      },
+    });
+  }
+
+  async update(
+    id: number,
+    { equipment_ID, unit, value }: MeasuringDataUpdatePutDTO,
+  ) {
+    await this.exists(id);
+
+    return await this.prisma.measurement.update({
+      data: {
+        equipment_ID,
+        unit,
+        value,
+      },
+      where: {
+        measurement_ID: id,
+      },
+    });
+  }
+
+  async updatePartial(
+    id: number,
+    { equipment_ID, unit, value }: MeasuringDataUpdatePatchDTO,
+  ) {
+    await this.exists(id);
+
+    const data: any = {};
+
+    if (equipment_ID) data.equipment_ID = equipment_ID;
+    if (unit) data.unit = unit;
+    if (value) data.value = value;
+
+    return await this.prisma.measurement.update({
+      data,
+      where: {
+        measurement_ID: id,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+    return await this.prisma.measurement.delete({
+      where: {
+        measurement_ID: id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (
+      !(await this.prisma.measurement.count({
+        where: {
+          measurement_ID: id,
+        },
+      }))
+    ) {
+      throw new BadRequestException(`Measuring data with id ${id} not found!`);
+    }
   }
 }
